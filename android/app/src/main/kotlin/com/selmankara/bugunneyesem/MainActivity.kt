26d9fbf5495082fc6910af59a51e5f2a17ffb44e
selmankara <selman@selmankara.com>
2021-02-05 13:57:57 +0300

    selman

---
+++ android/app/src/main/kotlin/com/selmankara/bugunneyesem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.selmankara.bugunneyesem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BenimUygulamam());
+}
+
+class BenimUygulamam extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            'BUGÜN NE YESEM',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: YemekSayfasi(),
+      ),
+    );
+  }
+}
+
+class YemekSayfasi extends StatefulWidget {
+  @override
+  _YemekSayfasiState createState() => _YemekSayfasiState();
+}
+
+class _YemekSayfasiState extends State<YemekSayfasi> {
+  @override
+  int corbaNo = 1;
+  int yemekNo = 1;
+  int tatliNo = 1;
+
+  List<String> corbaAdlari = [
+    'Mercimek Çorbası',
+    'Tarhana Çorbası',
+    'Tavuk Suyu',
+    'Düğün Çorbası',
+    'Yoğurtlu Çorba'
+  ];
+
+  List<String> yemekAdlari = [
+    'Karnıyarık',
+    'Mantı',
+    'Kuru Fasülye',
+    'İçli Köfte',
+    'Izgara Balık'
+  ];
+
+  List<String> tatliAdlari = [
+    'Kadayıf',
+    'Baklava',
+    'Sütlaç',
+    'Kazandibi',
+    'Dondurma'
+  ];
+
+  void yemekleriYenile() {
+    setState(() {
+      corbaNo = Random().nextInt(5) + 1;
+      yemekNo = Random().nextInt(5) + 1;
+      tatliNo = Random().nextInt(5) + 1;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: FlatButton(
+                  highlightColor: Colors.white,
+                  splashColor: Colors.white,
+                  onPressed: yemekleriYenile,
+                  child: Image.asset('assets/corba_$corbaNo.jpg')),
+            ),
+          ),
+          Text(
+            corbaAdlari[corbaNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: FlatButton(
+                  highlightColor: Colors.white,
+                  splashColor: Colors.white,
+                  onPressed: yemekleriYenile,
+                  child: Image.asset('assets/yemek_$yemekNo.jpg')),
+            ),
+          ),
+          Text(
+            yemekAdlari[yemekNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: FlatButton(
+                  highlightColor: Colors.white,
+                  splashColor: Colors.white,
+                  onPressed: yemekleriYenile,
+                  child: Image.asset('assets/tatli_$tatliNo.jpg')),
+            ),
+          ),
+          Text(
+            tatliAdlari[tatliNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
